e295da6b546265210da164047ca8b41dd33e97a6
huanminh254 <minhhuan110702@gmail.com>
2026-03-06 12:08:44 +0700

    Fix

--- app/src/main/java/com/example/lmslite/features/feature_grade/data/local/entity/GradeWithSubject.kt
+++ app/src/main/java/com/example/lmslite/features/feature_grade/data/local/entity/GradeWithSubject.kt
@@ -2,12 +2,11 @@ package com.example.lmslite.features.feature_grade.data.local.entity
 
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.example.lmslite.features.feature_grade.domain.model.Grade
 import com.example.lmslite.features.feature_student.data.local.entity.StudentEntity
 import com.example.lmslite.features.features_subject.data.local.entity.SubjectEntity
 
 data class GradeWithSubject(
-    @Embedded val grade: GradleEntity,
+    @Embedded val grade: GradeEntity,
     @Relation(parentColumn = "studentId", entityColumn = "id")
     val student: StudentEntity,
     @Relation(parentColumn = "subjectId", entityColumn = "id")
