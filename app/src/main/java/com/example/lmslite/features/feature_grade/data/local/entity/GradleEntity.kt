05da419c6189a3b71628769581ebdaa96d22c725
huanminh254 <minhhuan110702@gmail.com>
2026-03-06 11:58:14 +0700

    Fix

--- app/src/main/java/com/example/lmslite/features/feature_grade/data/local/entity/GradleEntity.kt
+++ app/src/main/java/com/example/lmslite/features/feature_grade/data/local/entity/GradleEntity.kt
@@ -3,26 +3,31 @@ package com.example.lmslite.features.feature_grade.data.local.entity
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-
+import com.example.lmslite.features.feature_student.data.local.entity.StudentEntity
+import com.example.lmslite.features.features_subject.data.local.entity.SubjectEntity
 
 @Entity(
     tableName = "grades",
     foreignKeys = [
         ForeignKey(
-            entity = GradleEntity::class,
+            entity = StudentEntity::class,
             parentColumns = ["id"],
             childColumns = ["idStudent"],
             onDelete = ForeignKey.CASCADE
         ),
         ForeignKey(
-            entity =
+            entity = SubjectEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["idSubject"],
+            onDelete = ForeignKey.CASCADE
         )
     ]
 )
-data class GradleEntity (
-    @PrimaryKey(autoGenerate = true) val id: Int? = 0,
-    val idSubject: Int?,
-    val idStudent: Int?,
+data class GradeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val idSubject: Int,
+    val idStudent: Int,
     val process: Float? = null,
     val final: Float? = null
 )
